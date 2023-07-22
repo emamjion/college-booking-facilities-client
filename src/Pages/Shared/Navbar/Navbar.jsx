1262935fee99c9f06d2396fec86f9d73f59ff945
emamjion <emamjion@gmail.com>
2023-07-22 11:34:25 +0600

    navbar section finished

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 // import React from 'react';
 
+import { Link, NavLink } from "react-router-dom";
+import './Navbar.css'
+
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar Here</h2>
-        </div>
+        <nav className="flex items-center justify-between px-24 pt-12">
+            <div>
+                <h2 className="text-2xl font-bold"><span className="text-[#f46565]">E</span>ducare</h2>
+            </div>
+            <ul>
+                <NavLink className='font-medium text-lg ml-12' to='/'>Home</NavLink>
+                <NavLink className='font-medium text-lg ml-12' to='/about'>About</NavLink>
+                <NavLink className='font-medium text-lg ml-12' to='/colleges'>Colleges</NavLink>
+                <NavLink className='font-medium text-lg ml-12' to='/admission'>Admission</NavLink>
+                <NavLink className='font-medium text-lg ml-12' to='/my-college'>My College</NavLink>
+            </ul>
+            <div>
+                <span>Profile</span>
+                <Link to='/login'>Login</Link>
+            </div>
+        </nav>
     );
 };
 
